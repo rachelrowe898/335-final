a895a13a27ac54078e3a38b91b0c9e5d610ce81b
sravyakommuri <sravyakommuri@gmail.com>
2022-12-13 01:58:11 -0500

    revised templates to take api parameter information

--- findFlightsValidation.js
+++ findFlightsValidation.js
@@ -1,33 +1,29 @@
 window.onsubmit = validateForm;
 
 function validateForm() {
+    let origin = document.getElementById("origin").value;
     let destination = document.getElementById("destination").value;
+    let numTickets = document.getElementById("numTickets").value;
 
     let month = document.getElementById("month").value;
     let day = document.getElementById("day").value;
 
-    let minPriceRange = document.getElementById("minPriceRange").value;
-    let maxPriceRange = document.getElementById("maxPriceRange").value;
-
     let daysInEachMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     let invalidMessages = "";
 
-    if(daysInEachMonth[month-1] != day) {
+    if(day > daysInEachMonth[month-1]) {
         invalidMessages += "Invalid date";
-    }
-
-    if(minPriceRange >= maxPriceRange) {
-        invalidMessages += "Invalid price range";
     }
 
     if (invalidMessages !== "") {
         alert(invalidMessages);
         return false;
     } else {
-        let valuesProvided = "Do you want to submit the following payment?\n";
+        let valuesProvided = "Do you want to submit the following flight details?\n";
+        valuesProvided += "Origin: " + origin + "\n";
         valuesProvided += "Destination: " + destination + "\n";
         valuesProvided += "Date: " + month + "/" + day + "\n";
-        valuesProvided += "Price Range: " + minPriceRange + "-" + maxPriceRange + "\n";
+        valuesProvided += "Number of Tickets: " + numTickets + "\n";
 
         return window.confirm(valuesProvided);
     }
